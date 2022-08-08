d9c5b8679e9ea5ea279532f36f8a7bb80ee66b6d
sylvain-grousset <68558176+sylvain-grousset@users.noreply.github.com>
2022-08-08 17:42:32 +0200

    Update : FontAwesome imports deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FuelPriceComponent } from './fuel-price/fuel-price.component';
@@ -21,7 +20,6 @@ import {MatCardModule} from "@angular/material/card";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FontAwesomeModule,
     BrowserAnimationsModule,
     MatCardModule
   ],
